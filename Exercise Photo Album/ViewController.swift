abe4a213cd903eda87b78cca138c3576f8e97333
yolanda halim <yolan9091@gmail.com>
2019-02-01 14:53:50 +0700

    Initial Commit

---
+++ Exercise Photo Album/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exercise Photo Album
+//
+//  Created by Yolanda Halim on 01/02/19.
+//  Copyright © 2019 Yolanda Halim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
